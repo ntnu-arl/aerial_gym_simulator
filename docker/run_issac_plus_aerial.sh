1a7e0fe331d0082c28528a5267e20f4f5c0fbdd4
Fausto M. Lagos S. <piratax007@protonmail.ch>
2023-12-22 12:55:41 +0100

    Improving file name

--- docker/run_issac_plus_aerial.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --network=host --gpus=all --name=isaacgym_container achilleas2942/issac-aerial-gym:latest /bin/bash
